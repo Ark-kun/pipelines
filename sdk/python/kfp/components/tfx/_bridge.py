e9cfad9213f502054d3c12f0768c7cfbbc8e32d7
Alexey Volkov <avolkov@google.com>
2020-03-22 23:16:45 -0700

    Bridge 2.1

--- sdk/python/kfp/components/tfx/_bridge.py
+++ sdk/python/kfp/components/tfx/_bridge.py
@@ -13,68 +13,67 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-###XXX### from __future__ import google_type_annotations
+"""This module provides simplified ways to create components."""
 
 __all__ = [
+    'create_component_from_func',
     'create_tfx_component_class_from_spec',
     'enable_new_components',
-    'InputUri',
-    'OutputUri',
+    'InputPath',
+    'OutputPath',
+    'InputArtifactUri',
+    'OutputArtifactUri',
 ]
 
 import contextlib
+import copy
 import json
 from typing import Any, Mapping, Type
 
+from kfp import components
 from kfp.components import create_component_from_func
 from kfp.components import InputPath
 from kfp.components import OutputPath
 from kfp.components import structures
-from kfp.components._components import _resolve_command_line_and_paths
-from kfp.components._naming import _sanitize_python_function_name
+# from kfp.components._components import _resolve_command_line_and_paths
+# from kfp.components._naming import _sanitize_python_function_name
+
+from google3.third_party.ml_metadata.proto import metadata_store_pb2
+from google3.third_party.tfx.components.base.base_component import BaseComponent
+from google3.third_party.tfx.components.base.executor_spec import ExecutorContainerSpec
+from google3.third_party.tfx.types import component_spec as tfx_component_spec
+from google3.third_party.tfx.types.artifact import Artifact
+from google3.third_party.tfx.types.channel import Channel
+from google3.third_party.tfx.types.component_spec import ChannelParameter
+from google3.third_party.tfx.types.standard_artifacts import ANY_ARTIFACT_TYPE_NAME
 
 
-###XXX###from google3.third_party.ml_metadata.proto.metadata_store_pb2 import ArtifactType
-###XXX###from google3.third_party.tfx.components.base.base_component import BaseComponent
-###XXX###from google3.third_party.tfx.components.base.executor_spec import ExecutorContainerSpec
-###XXX###from google3.third_party.tfx.types import component_spec as tfx_component_spec
-###XXX###from google3.third_party.tfx.types.artifact import Artifact
-###XXX###from google3.third_party.tfx.types.channel import Channel
-###XXX###from google3.third_party.tfx.types.component_spec import ChannelParameter
+def _create_artifact_type(type_struct) -> metadata_store_pb2.ArtifactType:
+  """Creates ArtifactType instance based on type_struct.
+  Args:
+    type_struct: The name of the artifact type. Default value is "Any",
+  """
 
-
-from ml_metadata.proto.metadata_store_pb2 import ArtifactType
-from tfx.components.base.base_component import BaseComponent
-from tfx.components.base.executor_spec import ExecutorContainerSpec
-from tfx.types import component_spec as tfx_component_spec
-from tfx.types.artifact import Artifact
-from tfx.types.channel import Channel
-from tfx.types.component_spec import ChannelParameter
-
-
-ANY_ARTIFACT_TYPE_NAME = 'Any'
-
-
-def _create_artifact_type(type_struct) -> ArtifactType:
+  # TODO(avolkov): Handle schema
   if not type_struct:
     type_name = ANY_ARTIFACT_TYPE_NAME
   elif isinstance(type_struct, str):
     type_name = str(type_struct)
   else:
     type_name = json.dumps(type_struct)
-  return ArtifactType(name=type_name)
+  return metadata_store_pb2.ArtifactType(name=type_name)
 
 
-def _create_channel_parameter(type_name: str) -> ChannelParameter:
-  return ChannelParameter(mlmd_artifact_type=_create_artifact_type(type_name))
+def _create_channel_parameter(type_struct) -> ChannelParameter:
+  return ChannelParameter(mlmd_artifact_type=_create_artifact_type(type_struct))
 
 
-def _create_empty_artifact(type_name: str) -> Artifact:
-  return Artifact(mlmd_artifact_type=_create_artifact_type(type_name))
+def _create_empty_artifact(type_struct) -> Artifact:
+  return Artifact(mlmd_artifact_type=_create_artifact_type(type_struct))
 
 
-def _create_channel_with_empty_artifact(type_name: str) -> Channel:
-  artifact_type = _create_artifact_type(type_name)
+def _create_channel_with_empty_artifact(type_struct) -> Channel:
+  artifact_type = _create_artifact_type(type_struct)
   return Channel(
       mlmd_artifact_type=artifact_type,
       artifacts=[
@@ -89,8 +88,12 @@ def type_to_type_struct(typ):
   return str(typ.__module__) + '.' + str(typ.__name__)
 
 
-class InputUri:
-  def __init__(self, data_type: type = None, type_struct: str = None):
+class InputArtifactUri(object):
+  """InputArtifactUri represents a URI pointing to readable data of certain type."""
+
+  def __init__(self, data_type: Type[Any] = None, type_struct: str = None):
+    if data_type and type_struct:
+      raise ValueError('Cannot specify data_type and type_struct at the same time')
     if type_struct:
       self._data_type_struct = type_struct
     else:
@@ -99,13 +102,25 @@ class InputUri:
   def to_dict(self):
     properties = {
         'data_type': self._data_type_struct,
-        'io_kind': 'read',
     }
-    return {'Uri': properties}
+    return {'InputArtifactUri': properties}
+
+  @staticmethod
+  def get_data_type(type_struct):
+    return type_struct['InputArtifactUri']['data_type']
+
+  @staticmethod
+  def type_struct_is_input_artifact_uri(type_struct) -> bool:
+    return type_struct == 'InputArtifactUri' or isinstance(
+        type_struct, dict) and type_struct.keys() == ['InputArtifactUri']
 
 
-class OutputUri:
-  def __init__(self, data_type: type = None, type_struct: str = None):
+class OutputArtifactUri(object):
+  """OutputArtifactUri represents a URI pointing to writable location for data of sertain type."""
+
+  def __init__(self, data_type: Type[Any] = None, type_struct: str = None):
+    if data_type and type_struct:
+      raise ValueError('Cannot specify data_type and type_struct at the same time')
     if type_struct:
       self._data_type_struct = type_struct
     else:
@@ -114,13 +129,23 @@ class OutputUri:
   def to_dict(self):
     properties = {
         'data_type': self._data_type_struct,
-        'io_kind': 'write',
     }
-    return {'Uri': properties}
+    return {'OutputArtifactUri': properties}
+
+  @staticmethod
+  def get_data_type(type_struct):
+    return type_struct['OutputArtifactUri']['data_type']
+
+  @staticmethod
+  def type_struct_is_output_artifact_uri(type_struct) -> bool:
+    return type_struct == 'OutputArtifactUri' or isinstance(
+        type_struct, dict) and type_struct.keys() == ['OutputArtifactUri']
 
 
-class ExecutionProperty:
-  def __init__(self, data_type: type = None, type_struct: str = None):
+class ExecutionProperty(object):
+  """ExecutionProperty represents a primitive input whose values might be queried in the future."""
+
+  def __init__(self, data_type: Type[Any] = None, type_struct: str = None):
     if type_struct:
       self._data_type_struct = type_struct
     else:
@@ -133,9 +158,16 @@ class ExecutionProperty:
     return {'QueryableProperty': properties}
 
 
+def _sanitize_python_class_name(name: str) -> str:
+  function_name = components._naming._sanitize_python_function_name(name)  # pylint: disable=protected-access
+  return ''.join(word.title() for word in function_name.split('_'))
+
+
 def create_tfx_component_class_from_spec(
     component_spec: structures.ComponentSpec,
 ) -> BaseComponent:
+  """Generates TFX components based on KFP-style ComponentSpec."""
+
   container_spec = component_spec.implementation.container
 
   if container_spec is None:
@@ -144,79 +176,70 @@ def create_tfx_component_class_from_spec(
     )
 
   input_name_to_python = {
-      input.name: _sanitize_python_function_name(input.name)
-      for input in component_spec.inputs or []
+      input_spec.name:
+      components._naming._sanitize_python_function_name(input_spec.name)  # pylint: disable=protected-access
+      for input_spec in component_spec.inputs or []
   }
   output_name_to_python = {
-      output.name: _sanitize_python_function_name(output.name)
-      for output in component_spec.outputs or []
+      output_spec.name:
+      components._naming._sanitize_python_function_name(output_spec.name)  # pylint: disable=protected-access
+      for output_spec in component_spec.outputs or []
   }
 
-  # FIX: The {{input_dict["{name}"][0].value}} placeholder is not implemented yet.
-  # @jxzheng and @avolkov are working on this.
-  component_arguments = {
-      input.name: '{{{{input_dict["{name}"][0].value}}}}'.format(
-          name=input_name_to_python[input.name])
-      for input in component_spec.inputs or []
-  }
-
-  # FIX: The {{input_dict["{name}"][0].local_path}} placeholder is not implemented yet.
-  # @avolkov is working on this.
-  def input_path_uri_generator(name):
-    #return '{{{{input_dict["{name}"][0].local_path}}}}'.format(name=input_name_to_python[name])
-    return '{{{{input_dict["{name}"][0].uri}}}}'.format(
-        name=input_name_to_python[name])
-
-  def output_path_uri_generator(name):
-    #return '{{{{output_dict["{name}"][0].local_path}}}}'.format(name=output_name_to_python[name])
-    return '{{{{output_dict["{name}"][0].uri}}}}'.format(
-        name=output_name_to_python[name])
-
-  resolved_cmd = _resolve_command_line_and_paths(
-      component_spec=component_spec,
-      arguments=component_arguments,
-      # FIX: Remove this workaround vvv
-      input_path_generator=input_path_uri_generator,
-      output_path_generator=output_path_uri_generator,
-  )
-
-  resolved_command = resolved_cmd.command
-  resolved_args = resolved_cmd.args
-
-  component_class_name = _sanitize_python_function_name(component_spec.name or
-                                                        'Component')
-  component_class_name = ''.join(
-      word.title() for word in component_class_name.split('_'))
-
-  component_class_doc = (component_spec.name or '') + '\n' + (
-      component_spec.description or '')
-
-  input_channel_parameters = {
-      input_name_to_python[input.name]:
-      _create_channel_parameter(input.type)
-      for input in component_spec.inputs or []
-  }
-
-  output_channel_parameters = {
-      output_name_to_python[output.name]:
-      _create_channel_parameter(output.type)
-      for output in component_spec.outputs or []
-  }
-
-  default_input_channels = {
-      input_name_to_python[input.name]:
-      _create_channel_with_empty_artifact(input.type)
-      for input in component_spec.inputs or []
-      if input.optional
-  }
-
-  output_channels = {
-      output_name_to_python[output.name]:
-      _create_channel_with_empty_artifact(output.type)
-      for output in component_spec.outputs or []
-  }
-
+  input_channel_parameters = {}
+  output_channel_parameters = {}
+  default_input_channels = {}
+  output_channels = {}
   execution_parameters = {}
+  inputs_that_are_input_artifact_uris = set()
+  inputs_that_are_output_artifact_uris = set()
+  inputs_that_are_execution_properties = set()
+
+  for input_spec in component_spec.inputs or []:
+    python_input_name = input_name_to_python[input_spec.name]
+
+    # Handling special cases like InputArtifactUri, OutputArtifactUri
+    if InputArtifactUri.type_struct_is_input_artifact_uri(input_spec.type):
+      inputs_that_are_input_artifact_uris.add(input_spec.name)
+      fixed_input = copy.copy(input_spec)
+      data_type = InputArtifactUri.get_data_type(input_spec.type)
+      fixed_input.type = {'Uri': {'data_type': data_type}}
+
+      input_channel_parameters[python_input_name] = _create_channel_parameter(
+          data_type)
+      if input_spec.optional:
+        default_input_channels[python_input_name] = (
+            _create_channel_with_empty_artifact(data_type))
+      continue
+
+    if OutputArtifactUri.type_struct_is_output_artifact_uri(input_spec.type):
+      inputs_that_are_output_artifact_uris.add(input_spec.name)
+      fixed_output = copy.copy(input_spec)
+      data_type = OutputArtifactUri.get_data_type(input_spec.type)
+      fixed_output.type = {'Uri': {'data_type': data_type}}
+      output_channel_parameters[python_input_name] = _create_channel_parameter(
+          data_type)
+      output_channels[python_input_name] = _create_channel_with_empty_artifact(
+          data_type)
+      continue
+
+    input_channel_parameters[python_input_name] = _create_channel_parameter(
+        input_spec.type)
+    if input_spec.optional:
+      default_input_channels[python_input_name] = (
+          _create_channel_with_empty_artifact(input_spec.type))
+
+  for output_spec in component_spec.outputs or []:
+    python_output_name = output_name_to_python[output_spec.name]
+    output_channel_parameters[python_output_name] = _create_channel_parameter(
+        output_spec.type)
+    output_channels[python_output_name] = _create_channel_with_empty_artifact(
+        output_spec.type)
+
+  component_name = component_spec.name or 'Component'
+  component_class_name = _sanitize_python_class_name(component_name)
+  component_class_doc = (component_spec.name or '') + '\n'
+  component_class_doc += (component_spec.description or '')
 
   tfx_component_spec_class = type(
       component_class_name + 'Spec',
@@ -228,16 +251,58 @@ def create_tfx_component_class_from_spec(
           __doc__=component_class_doc,
       ),
   )
-  print(tfx_component_spec_class.INPUTS)
+
+  component_arguments = {}
+  for input_spec in component_spec.inputs or []:
+    pythonic_input_name = input_name_to_python[input_spec.name]
+    if input_spec.name in inputs_that_are_input_artifact_uris:
+      cmd_argument = '{{{{input_dict["{name}"][0].uri}}}}'.format(
+          name=pythonic_input_name)
+    elif input_spec.name in inputs_that_are_output_artifact_uris:
+      cmd_argument = '{{{{output_dict["{name}"][0].uri}}}}'.format(
+          name=pythonic_input_name)
+    elif input_spec.name in inputs_that_are_execution_properties:
+      cmd_argument = '{{{{exec_properties["{name}"]}}}}'.format(
+          name=pythonic_input_name)
+    else:
+      # The {{input_dict["name"][0].value}} placeholder is not implemented yet.
+      # @jxzheng and @avolkov are working on this.
+      cmd_argument = '{{{{input_dict["{name}"][0].value}}}}'.format(
+          name=pythonic_input_name)
+    component_arguments[input_spec.name] = cmd_argument
+
+  # TODO(avolkov): Remove these generators once cl/294536017 is submitted
+  def input_path_uri_generator(name):
+    return '{{{{input_dict["{name}"][0].uri}}}}'.format(
+        name=input_name_to_python[name])
+
+  def output_path_uri_generator(name):
+    return '{{{{output_dict["{name}"][0].uri}}}}'.format(
+        name=output_name_to_python[name])
+
+  resolved_cmd = components._components._resolve_command_line_and_paths(  # pylint: disable=protected-access
+      component_spec=component_spec,
+      arguments=component_arguments,
+      # TODO(avolkov): Remove this workaround when cl/294536017 is submitted
+      input_path_generator=input_path_uri_generator,
+      output_path_generator=output_path_uri_generator,
+  )
+
+  resolved_command = resolved_cmd.command
+  resolved_args = resolved_cmd.args
 
   def tfx_component_class_init(self, **kwargs):
-    #instance_name = kwargs.pop('instance_name', None)
+    instance_name = kwargs.pop('instance_name', None)
     arguments = {}
     arguments.update(default_input_channels)
     arguments.update(output_channels)
     arguments.update(kwargs)
 
-    BaseComponent.__init__(self, spec=self.__class__.SPEC_CLASS(**arguments))
+    BaseComponent.__init__(
+        self,
+        spec=self.__class__.SPEC_CLASS(**arguments),
+        instance_name=instance_name,
+    )
 
   tfx_component_class = type(
       component_class_name,
@@ -262,17 +327,17 @@ def _create_tfx_task_from_component_spec_and_arguments(
     arguments: Mapping[str, Any],
     component_ref: structures.ComponentReference,
 ) -> BaseComponent:
+  del component_ref
   tfx_component_class = create_tfx_component_class_from_spec(component_spec)
   tfx_task = tfx_component_class(**arguments)
   return tfx_task
 
 
-class enable_new_components(contextlib.ContextDecorator):
+class enable_new_components(contextlib.ContextDecorator):  # pylint: disable=invalid-name
+
   def __enter__(self):
-    from kfp import components
-    self.old_handler = components._components._container_task_constructor
-    components._components._container_task_constructor = _create_tfx_task_from_component_spec_and_arguments
+    self.old_handler = components._components._container_task_constructor  # pylint: disable=protected-access
+    components._components._container_task_constructor = _create_tfx_task_from_component_spec_and_arguments  # pylint: disable=protected-access
 
   def __exit__(self, exc_type, exc, exc_tb):
-    from kfp import components
-    components._components._container_task_constructor = self.old_handler
+    components._components._container_task_constructor = self.old_handler  # pylint: disable=protected-access
--- sdk/python/kfp/components/tfx/_bridge_tests.py
+++ sdk/python/kfp/components/tfx/_bridge_tests.py
@@ -13,31 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for tfx.dsl.components._bridge."""
-
-#import sys
-#import subprocess
-#try:
-#  import kfp
-#except ModuleNotFoundError:
-#  #print(subprocess.run(['ls', '-la', '/usr/bin/']))
-#  print(subprocess.run(['find', '/', '-name', 'python3']))
-#  subprocess.run([sys.executable or 'python3', '-m', 'pip', 'install', 'kfp>=0.2.2', '--user'])
+"""Tests for google3.third_party.tfx.dsl.components._bridge."""
 
 import os
 
-from tfx.orchestration import metadata
-from tfx.orchestration import pipeline
-from tfx.orchestration import kubeflow
-from tfx.orchestration.kubeflow import kubeflow_dag_runner
-import tensorflow as tf
+import tensorflow  # pylint: disable=g-explicit-tensorflow-version-import
 
-###XXX###from tfx.dsl.components import create_component_from_func
-###XXX###from tfx.dsl.components import enable_new_components
-###XXX###from tfx.dsl.components import InputPath
-###XXX###from tfx.dsl.components import OutputPath
+from google3.third_party.tfx.dsl.components import create_component_from_func
+from google3.third_party.tfx.dsl.components import enable_new_components
+from google3.third_party.tfx.dsl.components import InputPath
+from google3.third_party.tfx.dsl.components import OutputPath
 
-from _bridge import create_component_from_func, enable_new_components, InputPath, OutputPath
+from google3.third_party.tfx.orchestration import metadata
+from google3.third_party.tfx.orchestration import pipeline
+from google3.third_party.tfx.orchestration.kubeflow import kubeflow_dag_runner
 
 
 # Python functions that we want to use in TFX pipeline
@@ -47,13 +36,17 @@ from _bridge import create_component_from_func, enable_new_components, InputPath
 # should be local file paths, not URIs. But we have to violate this rule until
 # data file passing is available. Until that, the components have to install
 # tensorflow and use tensorflow.gfile to read/write data.
-# TODO(avolkov): Simplify the components once data file passing support is added.
+
+# TODO(avolkov): Simplify the components once data file passing support is added
 
 
 # Reading data files
-def print_text_gfile(text_path: InputPath()): # Not specifying any type in InputPath so that any data can be printed
+def print_text_gfile(
+    # Not specifying any type in InputPath so that any data can be printed
+    text_path: InputPath(),
+):
   """Print text."""
-  import tensorflow as tf
+  import tensorflow.compat.v2 as tf  # pylint: disable=g-import-not-at-top
   with tf.io.gfile.GFile(text_path, 'r') as reader:
     for line in reader:
       print(line, end='')
@@ -64,7 +57,7 @@ def produce_1000_numbers_gfile(
     numbers_path: OutputPath('List of numbers'),  # Custom type
 ):
   """Produces numbers from 0 to 999."""
-  import tensorflow as tf
+  import tensorflow.compat.v2 as tf  # pylint: disable=g-import-not-at-top
   with tf.io.gfile.GFile(numbers_path, 'w') as writer:
     for i in range(1000):
       writer.write(str(i) + '\n')
@@ -72,11 +65,11 @@ def produce_1000_numbers_gfile(
 
 # Reading and writing data files
 def split_text_lines_gfile(
-    source_path: InputPath(), # InputUri()
-    odd_lines_path: OutputPath(), # OutputUri()
-    even_lines_path: OutputPath(), # OutputUri()
+    source_path: InputPath(),  # InputArtifactUri()
+    odd_lines_path: OutputPath(),  # OutputArtifactUri()
+    even_lines_path: OutputPath(),  # OutputArtifactUri()
 ):
-  import tensorflow as tf
+  import tensorflow.compat.v2 as tf  # pylint: disable=g-import-not-at-top
   with tf.io.gfile.GFile(source_path, 'r') as reader:
     with tf.io.gfile.GFile(odd_lines_path, 'w') as odd_writer:
       with tf.io.gfile.GFile(even_lines_path, 'w') as even_writer:
@@ -95,16 +88,16 @@ def split_text_lines_gfile(
 def create_text_split_gfile_pipeline_tasks():
   # These calls can be switched to decorators for simpler python components
   produce_1000_numbers_gfile_op = create_component_from_func(
-    produce_1000_numbers_gfile,
-    base_image='tensorflow/tensorflow:2.0.1-py3', # Needed for GFile
+      produce_1000_numbers_gfile,
+      base_image='tensorflow/tensorflow:2.0.1-py3',  # Needed for GFile
   )
   split_text_lines_gfile_op = create_component_from_func(
-    split_text_lines_gfile,
-    base_image='tensorflow/tensorflow:2.0.1-py3', # Needed for GFile
+      split_text_lines_gfile,
+      base_image='tensorflow/tensorflow:2.0.1-py3',  # Needed for GFile
   )
   print_text_gfile_op = create_component_from_func(
-    print_text_gfile,
-    base_image='tensorflow/tensorflow:2.0.1-py3', # Needed for GFile
+      print_text_gfile,
+      base_image='tensorflow/tensorflow:2.0.1-py3',  # Needed for GFile
   )
 
   produce_numbers_task = produce_1000_numbers_gfile_op()
@@ -113,32 +106,34 @@ def create_text_split_gfile_pipeline_tasks():
   )
   print_odd_lines_task = print_text_gfile_op(
       split_numbers_task.outputs['odd_lines'],
-      #instance_name='print_odd_lines',
+      # instance_name='print_odd_lines',
   )
-  #print_even_lines_task = print_text_gfile_op(
-  #    split_numbers_task.outputs['even_lines'],
-  #    instance_name='print_even_lines',
-  #)
-  #RuntimeError: Duplicated component_id PrintTextGfile for component type __main__.PrintTextGfil
-  #TypeError: Print text gfile() got an unexpected keyword argument 'instance_name'
+  # RuntimeError: Duplicated component_id PrintTextGfile for component type
+  # __main__.PrintTextGfile
+  # print_even_lines_task = print_text_gfile_op(
+  #     split_numbers_task.outputs['even_lines'],
+  # # TypeError: Print text gfile() got an unexpected keyword argument
+  # # 'instance_name'
+  # #     instance_name='print_even_lines',
+  # )
 
   tasks = [
       produce_numbers_task,
       split_numbers_task,
       print_odd_lines_task,
-      #print_even_lines_task,
+      # print_even_lines_task,
   ]
   return tasks
 
 
-class ComponentBridgeTests(tf.test.TestCase):
+class ComponentBridgeTests(tensorflow.test.TestCase):
 
   def setUp(self):
     super(ComponentBridgeTests, self).setUp()
     self._test_dir = os.path.join(
         os.environ.get('TEST_UNDECLARED_OUTPUTS_DIR', self.get_temp_dir()),
         self._testMethodName)
-    
+
     self._test_dir = os.getcwd()
 
     self._pipeline_name = 'component_bridge_test'
@@ -146,12 +141,6 @@ class ComponentBridgeTests(tf.test.TestCase):
                                        self._pipeline_name)
     self._metadata_path = os.path.join(self._test_dir, 'tfx', 'metadata',
                                        self._pipeline_name, 'metadata.db')
-
-    self._pipeline_root = os.path.join('gs://avolkov/tmp/' + self._test_dir, 'tfx', 'pipelines',
-                                       self._pipeline_name)
-    self._metadata_path = os.path.join('gs://avolkov/tmp/' + self._test_dir, 'tfx', 'metadata',
-                                       self._pipeline_name, 'metadata.db')
-
 
   def test_python_component_pipeline_with_kubeflow_runner(self):
     pipeline_func = create_text_split_gfile_pipeline_tasks
@@ -165,46 +154,20 @@ class ComponentBridgeTests(tf.test.TestCase):
         additional_pipeline_args={},
     )
 
-    #pipeline_package_filename = self._pipeline_name + '.tar.gz'
     pipeline_package_filename = self._pipeline_name + '.yaml'
     pipeline_pachage_path = os.path.join(
         self._test_dir,
         pipeline_package_filename
     )
 
-    # XXX
-    from tfx.orchestration.kubeflow.proto import kubeflow_pb2
-
-    metadata_config = kubeflow_pb2.KubeflowMetadataConfig(
-      grpc_config=kubeflow_pb2.KubeflowGrpcMetadataConfig(
-        #grpc_service_host=kubeflow_pb2.ConfigValue(value='metadata-service'),
-        grpc_service_host=kubeflow_pb2.ConfigValue(value='10.15.251.250'),
-        grpc_service_port=kubeflow_pb2.ConfigValue(value='8080'),
-      ) 
-    )
-
-    runner_config = kubeflow_dag_runner.KubeflowDagRunnerConfig(
-        #kubeflow_metadata_config=kubeflow_dag_runner.get_default_kubeflow_metadata_config(),
-        kubeflow_metadata_config=metadata_config,
-        # Specify custom docker image to use.
-        #tfx_image=tfx_image,
-        tfx_image='gcr.io/avolkov-31337/tensorflow/tfx:0.22.dev2', # Artifact Fixes
-        #supported_launcher_classes=[
-        #    in_process_component_launcher.InProcessComponentLauncher,
-        #    kubernetes_component_launcher.KubernetesComponentLauncher,
-        #],
-        #default_component_configs=[k8s_config],
-    )
-
-    runner = kubeflow.kubeflow_dag_runner.KubeflowDagRunner(
+    runner = kubeflow_dag_runner.KubeflowDagRunner(
         output_dir=self._test_dir,
         output_filename=pipeline_package_filename,
-        config=runner_config, #
     )
     runner.run(tast_pipeline)
 
     self.assertTrue(os.path.exists(pipeline_pachage_path))
-    self.assertTrue(tf.io.gfile.exists(pipeline_pachage_path))
+    self.assertTrue(tensorflow.io.gfile.exists(pipeline_pachage_path))
 
-if __name__ == "__main__":
-  tf.test.main()
+if __name__ == '__main__':
+  tensorflow.test.main()
